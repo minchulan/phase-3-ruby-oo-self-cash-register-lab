30874aa2e7c56f88c6700f5088f5a29541ea7b21
Minchul An <min@minchulan.io>
2023-01-31 16:33:37 -0700

    commit tree d20a1e0715304b0e1bfd9372729ce3bbd580d7ef

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,26 @@
+class CashRegister 
 
+    def initialize(discount)
+        @employee_discount = discount 
+        @total = 0
+    end 
+
+    def total
+        @total 
+    end 
+
+    def add_item(title, price)
+        @total 
+    end 
+
+    def apply_discount
+    end 
+
+    def items 
+    end 
+
+    def void_last_transaction
+    end 
+end 
+
+c1 = CashRegister.new(0.10)
